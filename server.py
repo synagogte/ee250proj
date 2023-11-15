e4231086099f7ff9923c432e9f29af61f16747cc
shri-kris <shrikris@usc.edu>
2023-11-15 14:23:00 -0800

    fixes

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from flask import jsonify
 from flask import request
 import json
 
+#TODO spotify api calls, create a database that has the songs and their duration / energy
+
+
 app = Flask('Test')
 
 num_duration = 0
@@ -12,17 +15,20 @@ num_energy = 0
 @app.route('/', methods=['GET'])
 def test1():
     return "duration: " + str(num_duration) + " energy: " + str(num_energy)
+    #TODO return the song that was selected
 
 #handles incoming post request
 @app.route('/post_test', methods=['POST', 'GET'])
 def receive_string():
+    #need to declare that these are globals
     global num_duration
     global num_energy
+
     # Extract the data from the POST request
     duration = request.json.get('duration')
     energy = request.json.get('energy')
 
-    #These are 'not accessed'
+    #TODO: use num_duration and num_energy which come from the rpi sensors to select a song.
     num_duration = float(duration)
     num_energy = float(energy)
 
